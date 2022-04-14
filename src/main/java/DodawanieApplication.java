919e823cf0f25a20bf49d31faf99c97ec5db7035
lukazam890 <72615076+lukazam890@users.noreply.github.com>
2022-04-14 13:53:15 +0200

    Initial commit

---
+++ src/main/java/DodawanieApplication.java
@@ -0,0 +1,10 @@
+public class DodawanieApplication {
+    public static void main(String[] args)
+    {
+
+    }
+    public int added(int a, int b)
+    {
+        return a*b;
+    }
+}
---
+++ src/test/java/DodawanieApplicationTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+public class DodawanieApplicationTest {
+
+    @org.junit.Test
+    public void added() {
+        DodawanieApplication dodawanieApplication=new DodawanieApplication();
+        assertEquals(10,dodawanieApplication.added(5,5));
+    }
+}
